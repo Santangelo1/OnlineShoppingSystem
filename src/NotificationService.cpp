69a42db28e320dbdf2cf1155fefa8abd504e2c5f
Nancy <Nancy.berry@ucdenver.edu>
2024-11-07 19:32:34 +0000

    Nov 7, 2024, 12:32 PM

--- src/NotificationService.cpp
+++ src/NotificationService.cpp
@@ -7,10 +7,9 @@ NotificationService::NotificationService() {}
 // Method to check for low stock in the inventory
 void NotificationService::checkLowStock(const Inventory &inventory, int threshold) const
 {
-
-    //Access products via getProducts
-    for (const auto& product : inventory.getProducts())
-    { // Assuming `getProducts` returns a vector of all products
+    // Access products via getProducts
+    for (const auto &product : inventory.getProducts())
+    {
         if (product.getStockQuantity() <= threshold)
         {
             std::string message = "Low stock alert: " + product.getName() + " (ID: " + std::to_string(product.getId()) +
