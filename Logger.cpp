f3c773a082c3f085e938838a521bae56aaffb7d5
Nanci Berry <178339397+Santangelo1@users.noreply.github.com>
2024-11-25 16:07:51 -0700

    Update Logger.cpp

--- Logger.cpp
+++ Logger.cpp
@@ -2,17 +2,43 @@
 #include <iostream>
 #include <fstream>
 
+//Define the static instance variable
 Logger& Logger::getInstance() {
-    static Logger instance;
+    static Logger instance;  //Static instance of the logger
     return instance;
 }
 
-void Logger::log(const std::string& message) {
-    std::ofstream file("log.txt", std::ios::app); // Open log file in append mode
-    if (file.is_open()) {
-        file << message << "\n"; // Write message to file
-        file.close();
-    } else {
-        std::cerr << "Unable to open log file.\n"; // Print error if file cannot be opened
+//Constructor
+Logger::Logger() {
+file.open("log.txt", std::ios::app); //Open log file in append mode
+if (!file.is_open()) {
+    std::cerr << "Unable to open log file.\n";
+//Print error if file cannot be opened.
+}
+}
+
+//Destructor
+Logger::~Logger() {
+    if (!file.is_open()) {
+        file.close(); //Close the log file if it was opened
     }
 }
+
+//Log a message to the file
+void Logger::log(const std::string& message) {
+    if (file.is_open()) {
+        file << message <<  "\n";  //Write message to file
+    } else {
+        std::cerr << "Log file is not open. Unable to log message.\n";
+        //Handle logging failure
+    }
+}
+
+//Prevent copying
+Logger::Logger(const Logger&) = delete;
+Logger& Logger::operator=(const Logger&) = delete;
+
+
+
+
+
